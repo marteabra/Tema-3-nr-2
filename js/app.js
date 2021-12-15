245c8075bb7fccf5c6fbcac1f25d9e8d274e9815
marteabra <marte.abrahams1@hotmail.com>
2021-12-15 13:31:38 +0100

    alt fungerer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function init() {
     const plusEl = document.querySelector('.plus');
-    const mobilNav = document.querySelector('.nav-links');
+    const mobilNav = document.querySelector('.nav');
     plusEl.addEventListener('click', () => {
         mobilNav.classList.toggle('nav-links-expand');
     });
